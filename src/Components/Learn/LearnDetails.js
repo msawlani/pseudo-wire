32ff3705d3eac908d74fd60fd52bb42baedb1613
Michael <39811434+msawlani@users.noreply.github.com>
2022-10-19 15:59:15 -0400

    example code and editor
    added example code into the editor using redux
    
    added editor into learn details

--- src/Components/Learn/LearnDetails.js
+++ src/Components/Learn/LearnDetails.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import CodeEditor from "../../Shared/Editor/Editor";
 
 const LearnDetails = (props) => {
   let { link_id } = useParams();
@@ -11,7 +12,15 @@ const LearnDetails = (props) => {
 
   return (
     <div>
-      <h1>{item.language}</h1>
+      <div>
+        <h1 className="text-white">{item.language}</h1>
+      </div>
+      <CodeEditor
+        readOnly={true}
+        scroll={false}
+        language={item.language}
+        content={item?.content}
+      />
     </div>
   );
 };
--- src/Redux/Reducers.js
+++ src/Redux/Reducers.js
@@ -1,6 +1,14 @@
 const initState = {
   links: [
-    { id: 0, language: "Javascript" },
+    {
+      id: 0,
+      language: "Javascript",
+      content: `let array = ["H", "e", "l", "l", "o"]; 
+
+  for (let i = 0; i < array.length; i++ ){
+        console.log(array[i]);
+  }`,
+    },
     { id: 1, language: "Typescript" },
     { id: 2, language: "Java" },
   ],
